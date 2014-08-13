0d1d5334d22ba850e62d3ef5b6b0bbe7a09017eb
liudao1991 <416333021@qq.com>
2014-08-13 12:06:38 +0800

    webQQ

---
+++ webQQ/js/index.js
@@ -0,0 +1,27 @@
+$(function(){
+
+// 	$(window).scroll( function() { 
+//     alert("滚动条到顶部的垂直高度: "+$(document).scrollTop()); 
+//     alert("页面的文档高度 ："+$(document).height());
+//     alert('浏览器的高度：'+$(window).height());
+//     alert($(window).height()); //浏览器时下窗口可视区域高度
+// 	   alert($(document).height()); //浏览器时下窗口文档的高度
+//     alert($(document.body).height());//浏览器时下窗口文档body的高度
+//     alert($(document.body).outerHeight(true));//浏览器时下窗口文档body的总高度 包括border padding margin
+//     alert($(window).width()); //浏览器时下窗口可视区域宽度
+//     alert($(document).width());//浏览器时下窗口文档对于象宽度
+//     alert($(document.body).width());//浏览器时下窗口文档body的高度
+//     alert($(document.body).outerWidth(true));//浏览器时下窗口文档body的总宽度 包括border padding margin
+
+//     alert($(document).scrollTop()); //获取滚动条到顶部的垂直高度
+//     alert($(document).scrollLeft()); //获取滚动条到左边的垂直宽度
+// });
+
+	//alert('浏览器的高度：'+$(window).height());
+	$(window).height(function(){
+		$(".black").css("height",$(window).height());
+	});
+	
+
+
+})
