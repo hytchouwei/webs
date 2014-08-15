319892da273bd41048dab5a29dec081ef48d1b77
liudao1991 <416333021@qq.com>
2014-08-15 23:36:57 +0800

    webQQ5.0

--- webQQ/js/index.js
+++ webQQ/js/index.js
@@ -39,6 +39,37 @@ $(function(){
 		$("#panel"+n).show();
 	});
 
+	$(".listItem").click(function(){
+		var num = $(this).attr("class").split("Ltqq");
+		var name = $(this).find(".memberNick").html().split("<span>");
+		
+		if($(this).attr("showchatPanel") == "false")
+		{
+			addChatPanel(num[1],name[0]);
+			$(".Ltqq"+num[1]).attr("showchatPanel","true")
+		}
+		
+		$(".chatPanel").css("z-index",0);
+		
+		$(".chatPanel"+num[1]).css("z-index",1);
+		
+	});
+	$(document).on("click",".chatHeaderRightButton",function(){
+		var numC = $(this).parent().parent().attr("class").split("chatPanel chatPanel");
+		$(".Ltqq"+numC[1]).attr("showchatPanel","false");
+
+		$(this).parent().parent().remove();
+	});
+
+	$(document).on("click",".sendBtn",function(){
+		var numC = $(this).parent().parent().parent().attr("class").split("chatPanel chatPanel");
+		
+		var content = $(this).parent().find(".chatTextarea").val();
+
+		send(numC[1],content);
+
+		$(this).parent().find(".chatTextarea").val("");
+	});
 
 });
 
@@ -47,3 +78,46 @@ $(function(){
  		'url("images/tab_icon_plugin_selected.png")',
  		'url("images/tab_icon_setup_selected.png")'
  	);
+
+ function addChatPanel(num,name){
+ 	var html="";
+ 	html+='			<div class="chatPanel chatPanel'+num+'">';
+ 	html+='				<div class="chatHeader">';
+ 	html+='					<div class="chatHeaderLeftButton">';
+ 	html+='						<span class="chatHeaderLeftButtonI"></span>';
+ 	html+='					</div>';
+ 	html+='					<h1 class="chatTitle">'+name+'</h1>';
+ 	html+='					<div class="chatHeaderRightButton">';
+ 	html+='						<span class="chatHeaderRightButtonI">关闭</span>';
+ 	html+='					</div>';
+ 	html+='				</div>';
+ 	html+='				<div class="chatBodyWrapper">';
+ 	html+='					<div class="chatBodyBox">';
+ 	html+='					</div>';
+ 	html+='				</div>';
+ 	html+='				<div class="chatFooter">';
+ 	html+='					<div class="chatToolbar">';
+ 	html+='						<div class="addFaceBtn">';
+ 	html+='							<span class="btnImg"></span>';
+ 	html+='						</div>';
+ 	html+='						<textarea class="chatTextarea"></textarea>';
+ 	html+='						<button class="sendBtn">';
+ 	html+='							<span>发送</span>';
+ 	html+='						</button>';
+ 	html+='					</div>';
+ 	html+='				</div>';
+ 	html+='			</div>';
+ 	$("#container").append(html);
+ }
+
+
+ function send(n,c){
+ 	var html="";
+ 	html+='						<div class="chatContentSelf">';
+ 	html+='							<img class="avatarImg" src="images/g (1).jpg">';
+ 	html+='							<p class="chatNick">六道';
+ 	html+='							</p>';
+ 	html+='							<p class="chatContent ">'+c+'</p>';
+ 	html+='						</div>';
+ 	$(".chatPanel"+n).find(".chatBodyBox").append(html);
+ }
