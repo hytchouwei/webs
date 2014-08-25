e502ed5197f8f21b1f0cf01670a081d770fe5e53
liudao1991 <416333021@qq.com>
2014-08-25 22:31:39 +0800

    ITXY.FeiGou

---
+++ ITXY.FeiGou/ITXY.FeiGou/MainForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ITXY.FeiGou
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.textHistory = new System.Windows.Forms.TextBox();
+            this.textMsg = new System.Windows.Forms.TextBox();
+            this.textUser = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(350, 334);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // textHistory
+            // 
+            this.textHistory.Location = new System.Drawing.Point(23, 12);
+            this.textHistory.Multiline = true;
+            this.textHistory.Name = "textHistory";
+            this.textHistory.ReadOnly = true;
+            this.textHistory.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textHistory.Size = new System.Drawing.Size(402, 288);
+            this.textHistory.TabIndex = 1;
+            // 
+            // textMsg
+            // 
+            this.textMsg.Location = new System.Drawing.Point(23, 334);
+            this.textMsg.Name = "textMsg";
+            this.textMsg.Size = new System.Drawing.Size(307, 21);
+            this.textMsg.TabIndex = 1;
+            // 
+            // textUser
+            // 
+            this.textUser.Location = new System.Drawing.Point(441, 12);
+            this.textUser.Multiline = true;
+            this.textUser.Name = "textUser";
+            this.textUser.ReadOnly = true;
+            this.textUser.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textUser.Size = new System.Drawing.Size(100, 288);
+            this.textUser.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 378);
+            this.Controls.Add(this.textUser);
+            this.Controls.Add(this.textMsg);
+            this.Controls.Add(this.textHistory);
+            this.Controls.Add(this.btnSend);
+            this.Name = "MainForm";
+            this.Text = " 六道聊天";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox textHistory;
+        private System.Windows.Forms.TextBox textMsg;
+        private System.Windows.Forms.TextBox textUser;
+    }
+}
+
---
+++ ITXY.FeiGou/ITXY.FeiGou/MainForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace ITXY.FeiGou
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            send(null);
+            this.textMsg.Text = "";
+        }
+
+        //线程
+        private void lis()
+        {
+            //接收
+            UdpClient uc = new UdpClient(9527);
+            while (true)
+            {
+                IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 0);
+                byte[] bmsg = uc.Receive(ref ipep);
+                string msg = Encoding.Default.GetString(bmsg);
+                string[] msgN = msg.Split('|');
+                if (msgN[0] == "PUBLIC")
+                {
+                    this.textHistory.Text += msgN[2] + ":"  + msgN[1] + "\r\n";
+                }
+                if (msgN[0] == "INROOM")
+                {
+                    this.textUser.Text += msgN[1] + "\r\n";
+                }
+                if (msgN[0] == "OUTROOM")
+                {
+                    this.textUser.Text = this.textUser.Text.Replace(msgN[1] + "\r\n","");
+                }
+
+            } 
+        }
+        private Thread th;
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            MainForm.CheckForIllegalCrossThreadCalls = false;
+            th = new Thread(new ThreadStart(lis));
+            th.IsBackground = true;
+            th.Start();
+            send("login");
+        }
+        //发送
+        private void send(string m)
+        {
+            string ip = "255.255.255.255";
+            UdpClient uc = new UdpClient();
+            string msg="空";
+            if (this.textMsg.Text != "")
+            {
+                msg = "PUBLIC|" + this.textMsg.Text + "|六道";
+            }
+            if (m == "over")
+            {
+                msg = "OUTROOM|六道";
+            }
+            if (m == "login")
+            {
+                msg = "INROOM|六道";
+            }
+            if (msg != "空")
+            {
+                byte[] bmsg = Encoding.Default.GetBytes(msg);
+                IPEndPoint ipep = new IPEndPoint(IPAddress.Parse(ip), 9527);
+                uc.Send(bmsg, bmsg.Length, ipep);
+            }
+            
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            th.Abort();
+            send("over");
+        }
+    }
+}
